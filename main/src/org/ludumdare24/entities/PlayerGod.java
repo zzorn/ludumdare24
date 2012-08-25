86fd6056087ad565c42e01d665e54436eb423fa5
shiera <anon>
2012-08-25 15:53:07 +0300

    particleefeccts

--- main/src/org/ludumdare24/entities/PlayerGod.java
+++ main/src/org/ludumdare24/entities/PlayerGod.java
@@ -1,10 +1,12 @@
 package org.ludumdare24.entities;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.Table;
 import org.gameflow.screen.Screen2D;
 
@@ -16,6 +18,8 @@ public class PlayerGod extends God {
     private Label manaLabel;
 
     private Tool currentTool;
+
+    private ParticleEffect cursorEffect=null;
 
 
     @Override
@@ -31,23 +35,50 @@ public class PlayerGod extends God {
 
         // Action buttons
         Table buttons = new Table();
-        buttons.add(screen2D.createButton("Smite", new ClickListener() {
+        TextButton smiteButton = screen2D.createButton("Smite", new ClickListener() {
             public void click(Actor actor, float x, float y) {
-                currentTool = Tool.SMITE;
+                changeTool(Tool.SMITE);
             }
-        }));
+        });
+
+        buttons.add(smiteButton);
+
         buttons.add(screen2D.createButton("Love", new ClickListener() {
             public void click(Actor actor, float x, float y) {
-                currentTool = Tool.LOVE;
+                changeTool(Tool.LOVE );
             }
         }));
         hud.add(buttons).expand().bottom();
+
+        changeTool(null);
 
 
         // Add HUD to screen
         screen2D.getStage().addActor(hud);
 
         return super.create(screen2D);
+    }
+
+    private void changeTool(Tool tool) {
+        currentTool = tool;
+        cursorEffect = new ParticleEffect();
+        cursorEffect.load(Gdx.files.internal("particles/smitSelect.particle"),);
+        switch (currentTool ){
+            case SMITE:
+
+                break;
+            case LOVE:
+                break;
+            case MOVE:
+                break;
+            case TOTEM:
+                break;
+            default :
+                break;
+
+
+
+        }
     }
 
     @Override
