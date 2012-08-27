91814be09e2a83375b4bd07f33a3c23aef395910
Shiera <->
2012-08-27 20:39:13 +0300

    hadlad

---
+++ main/src/org/ludumdare24/world/TileMap.java
@@ -0,0 +1,11 @@
+package org.ludumdare24.world;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cesse
+ * Date: 27.8.2012
+ * Time: 20:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class TileMap {
+}
